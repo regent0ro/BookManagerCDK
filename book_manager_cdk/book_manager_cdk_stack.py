1ea283cb83c88b88145084e686bafc90a6336cee
regent0ro <26410322+regent0ro@users.noreply.github.com>
2025-03-20 06:01:13 +0000

    Add Book table

--- book_manager_cdk/book_manager_cdk_stack.py
+++ book_manager_cdk/book_manager_cdk_stack.py
@@ -1,19 +1,67 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+    aws_lambda as _lambda,
 )
 from constructs import Construct
+
 
 class BookManagerCdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # DynamoDB
+        book_table = dynamodb.TableV2(
+            self,
+            "Book",
+            partition_key=dynamodb.Attribute(
+                name="book_id", type=dynamodb.AttributeType.STRING),
+        )
+        # user_table = dynamodb.TableV2(
+        #     self,
+        #     "User",
+        #     partition_key=dynamodb.Attribute(name="user_id", type=dynamodb.AttributeType.STRING),
+        # )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "BookManagerCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
+        # lambda
+        # borrow
+        # borrow_book_lambda = _lambda.Function(
+        #     self,
+        #     "borrow_book",
+        #     runtime=_lambda.Runtime.Python_3_13,
+        #     code=_lambda.Code.from_asset("lambda"),
+        #     handler="borrow_book.handler",
+        #     environment={},
+        # )
+
+        # ## return
+        # return_book_lambda = _lambda.Function(
+        #     self,
+        #     "return_book",
+        #     runtime=_lambda.Runtime.Python_3_13,
+        #     code=_lambda.Code.from_asset("lambda"),
+        #     handler="return_book.handler",
+        #     environment={},
+        # )
+
+        # # add
+        # add_book_lambda = _lambda.Function(
+        #     self,
+        #     "add_book",
+        #     runtime=_lambda.Runtime.PYTHON_3_13,
+        #     code=_lambda.Code.from_asset("book_manager_cdk/lib/lambda"),
+        #     handler="add_book.handler",
+        #     environment={},
+        # )
+
+        # ## delete
+        # delete_book_lambda = _lambda.Function(
+        #     self,
+        #     "delete_book",
+        #     runtime=_lambda.Runtime.Python_3_13,
+        #     code=_lambda.Code.from_asset("lambda"),
+        #     handler="delete_book.handler",
+        #     environment={},
         # )
